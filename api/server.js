aabc5dc79ffd1540b0a59bb1215a64b1035e18db
Xavier <etu51971@henallux.be>
2024-12-09 12:23:22 +0100

    logging controllers, séparation des fonctions de utils.js

--- api/server.js
+++ api/server.js
@@ -9,11 +9,13 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
+// Middleware d'erreur si tentative d'utilisation d'une version non implémentée
+app.use("/api/v:version", notSupportedAPIVersion);
+
 //placé après la route qui vérifie la verison
 app.use("/api/v1", RouterV1); // Routeur pour la version 1
 
-// Middleware d'erreur si tentative d'utilisation d'une version non implémentée
-app.use("/api/v:version", notSupportedAPIVersion);
+
 
 app.use(express.static('./upload'));
 
